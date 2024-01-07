cd8234e04b891dea1c0ad8eb376365485710019d
intrepid-codes <152840765+intrepid-codes@users.noreply.github.com>
2024-01-07 18:37:29 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        Login | Coolest Chatting Platform In India
+    </title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="https://cdn3d.iconscout.com/3d/premium/thumb/chat-2997147-2516185.png?f=webp" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.0.0/fonts/remixicon.css" rel="stylesheet" />
+</head>
+<body style="background-color: #dadada;">
+<div id="main">
+    <div id="page1">
+    <div id="cursor"></div>
+        <nav>
+            <div id="brand">
+                <img src="https://cdn3d.iconscout.com/3d/premium/thumb/chat-2997147-2516185.png?f=webp" alt="">
+                <div id="navline"></div>
+                <h2>Prichat</h2>
+
+                <div id="menu-opener">
+                    <div id="menuBox">
+                        <h4>Menu</h4>
+                        <div id="linkBox">
+                            <a href="index.php"><span>Home</span><span>Home</span></a>
+                            <a href="login.php"><span>Login</span><span>Login</span></a>
+                            <a href="signup.php"><span>Signup</span><span>Signup</span></a>
+                            <a href="contact.php"><span>Contact</span><span>Contact</span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="menu-button" onclick="animateMenu();">
+                <div class="line1"></div>
+                <div class="line2"></div>
+            </div>
+        </nav>
+        <div id="login-container">
+            <h3>Log in</h3>
+            <form>
+                <label for="Username">Email</label>
+                <input type="email" id="email" placeholder="email">
+                <label for="Username">Password</label>
+                <input type="text" id="password" placeholder="password">
+                <div id="checkbox">
+                <input type="checkbox" name="checkbox" id="checkbox">
+                <label for="checkbox">keep me logged in</label>
+                </div>
+                <button>Log in now</button>
+                <a href="forgot_password.php">Forgot your password ?</a>
+                <hr>
+                <h4>Or Sign In With -</h4>
+                <div id="google-login">
+                    <div id="google-button">
+                    <i class="ri-google-fill"></i>
+                    <h4>Sign in with Google</h4>
+                    </div>
+                </div>
+                <h4 id="register-btn">Don't have an account? <a href="signup.php">Register now</a></h4>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+<script>
+    function animateMenu(){
+    var menuBtn = document.querySelector('#menu-button')
+    var menuOpener = document.querySelector('#menu-opener')
+    var needBlur = document.querySelector('#login-container')
+    var needScale = document.querySelectorAll('#linkBox a')
+    var line1 = document.querySelector('.line1')
+    var line2 = document.querySelector('.line2')
+
+    if (line1.className == "line1" && line2.className == "line2") {
+        line1.className = "line1 rotate"
+        line2.className = "line2 rotates"
+        needBlur.style.WebkitFilter = "blur(2px)"
+        menuOpener.style.right = "0"
+    } else {
+        line1.className = "line1"
+        line2.className = "line2"
+        menuOpener.style.right = "-100%"
+        needBlur.style.WebkitFilter = "blur(0px)"
+        }
+}
+</script>
+</html>
